ab590583ed5289ef6fed8a0012c1c0fd94cde1c1
Marcos <marcintosh87@gmail.com>
2022-01-10 17:42:10 -0500

    commit tree 10458c20432027bd8c2bc5b801a6b349294807df

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { is: 10 }, allow_blank: false
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }, allow_blank: false
+  validates :summary, length: { maximum: 250 }, allow_blank: false
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']  }
+  validates :title, exclusion: { in: ['True Facts']  }
 end
