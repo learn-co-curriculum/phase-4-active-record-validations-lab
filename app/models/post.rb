17dc1562f35fa3168e19c36212bf7c175c0eec64
L. Cadet <85368055+Ludcad615@users.noreply.github.com>
2022-07-11 14:30:26 -0400

    commit tree fb8b99197af595b2e1a78c11130149510f7ff424

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait?
+  
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+  end
