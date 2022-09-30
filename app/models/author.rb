2e095c30afa17cd91295e338684cf44d575e8115
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-30 08:30:18 +0300

    commit tree 17c14698edfd149030dace2bff0b565711e83fd1

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :name, presence:true, uniqueness: true
+    validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait_y
+
+    def clickbait_y
+        if title
+            unless title.include?("Won't Believe") || title.include?("Secret") || title.match?(/Top\s\d/) || title.include?("Guess")
+                errors.add(:title, "Not clickbiat-y enough")
+            end
+        end
+    end
 end
