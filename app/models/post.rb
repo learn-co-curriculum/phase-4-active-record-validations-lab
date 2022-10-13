15eeac85627c91c42a41101a8ae1e8002a9038b5
Ricardo Guerra <RikkuG491@gmail.com>
2022-10-13 16:52:56 -0400

    commit tree 152a2ddedcd124f4a418bdcbd7f4d62dc058460d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait
+
+    def clickbait
+        unless(title.include?("Won't Believe") || title.include?("Secret") || /Top \d/i.match(title) || title.include?("Guess"))
+            errors.add(:title, "Must be clickbait")
+        end
+    end
 end
