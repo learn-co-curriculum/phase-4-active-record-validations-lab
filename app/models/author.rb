ef5464c40b61193916a1c183051388b46d980f98
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-06-22 15:17:19 -0700

    Active Record Validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true, clickbait: true
+    validates :content, length: {min: 250}
+    validates :summary, length: {max: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    private
+    def clickbait_title?
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        clickbait_phrases.any? { |phrase| title.include?(phrase) }
+    end
+
+    def clickbait_validator
+        errors.add(:title, 'is not clickbait-y enough') unless clickbait_title?
+      end
+      
+      validate :clickbait_validator
+    
 end
